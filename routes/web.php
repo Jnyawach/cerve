9298703c9ca6f1e8ef62488e5cf4a1435aa0c84c
jnyawach <nyawach41@gmail.com>
2020-09-20 10:29:57 +0300

    role

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware'=>'RoleMiddleware'], function(){
+Route::group(['middleware'=>'role'], function(){
 
     Route::resource('/admin','AdminController');
     Route::resource('admin/homepage/users','UserAdminController');
